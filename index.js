c28564299905bec6e733bdbf0e48c2a557fbb5fc
Ferclemens <69605971+Ferclemens@users.noreply.github.com>
2023-07-17 19:11:08 -0300

    add like function

--- index.js
+++ index.js
@@ -30,14 +30,10 @@ const posts = [
     }
 ]
 
-
-let postsCounter = 0
-
 function setPosts(array) {
     let render = ''
 
     for(let i = 0; i < array.length; i++) {
-        postsCounter += 1
         render += 
         `<div class="card">
             <div class="card-header">
@@ -51,11 +47,11 @@ function setPosts(array) {
             <img class="post-heart" id="heart-el${i}" src="images/icon-heart-active.png">    
             <div class="description--container">
                 <div class="buttons-container">
-                    <button class="like-btn" id="${i}"><img src="images/icon-heart.png" id="heart-img" ></button>
+                    <button class="like-btn" id="${i}"><img src="images/icon-heart.png" id="heart-img${i}" ></button>
                     <button><img src="images/icon-comment.png"></button>
                     <button><img src="images/icon-dm.png"></button>
                 </div>
-                <strong><p>${array[i].likes} likes</p></strong>
+                <strong><p id="likesItem${i}">${array[i].likes} likes</p></strong>
                 <strong><p>${array[i].username}</strong> ${array[i].comment}</p>
             </div>
             <hr class="separator">
@@ -67,22 +63,35 @@ function setPosts(array) {
 //references
 const postsEl = document.getElementById('posts-el')
 
+//add posts to html
 const postsList =setPosts(posts)
 postsEl.innerHTML = postsList
  
-
-
 const lista = document.getElementsByClassName('like-btn')
 
 for(let i = 0; i < lista.length; i++) {
     let element = document.getElementById(`heart-el${i}`)
     let heartImgEl = document.getElementById(`heart-img${i}`)
+    let likesEl = document.getElementById(`likesItem${i}`)
     lista[i].addEventListener('click', function(){
-        //console.log('ID?',i);
-        //console.log('ID heart', element);
+        const activeHeart = 'images/icon-heart-active.png'
+        const emptyHeart = 'images/icon-heart.png'
+        
+        if(heartImgEl.src != activeHeart){
+            heartImgEl.src = 'images/icon-heart-active.png';
+            posts[i].likes += 1;
+            likesEl.innerHTML = `${posts[i].likes} likes`
+        }
+        else if(heartImgEl.src == activeHeart) {
+            heartImgEl.src = 'images/icon-heart.png';
+            posts[i].likes -= 1;
+            likesEl.innerHTML = `${posts[i].likes} likes`
+        }
+        //set heart in the post
         element.style.display = 'block'
         setTimeout(() => {
             element.style.display = 'none'
         },1000)
+       
     })
 }
